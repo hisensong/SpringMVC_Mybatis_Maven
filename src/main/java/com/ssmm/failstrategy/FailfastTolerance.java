53547be6406abc9544c20013b1ba1930966bed0d
HisenSong <songhaizhen@gomeholdings>
2016-05-18 19:12:50 +0800

    添加具体策略类

--- src/main/java/com/ssmm/failstrategy/FailfastTolerance.java
+++ src/main/java/com/ssmm/failstrategy/FailfastTolerance.java
@@ -9,8 +9,12 @@ public class FailfastTolerance extends AbstractFailTolerance{
 
 	@Override
 	public <S, T, P> T invoke(S object, String methodName, P parameters) {
+		try{
+			return invokeMethod(object, methodName, parameters);
+		}catch(Exception e){
+			 throw new RuntimeException(e.getMessage(), e);
+		}
 		
-		return null;
 	}
 
 }
